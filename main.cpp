342cc60650265c316caf43b9fe5c0b4da47fde73
octavi26 <dimitrie-octavian.radu@s.unibuc.ro>
2025-05-04 16:47:31 +0300

    Fix missing reference in Value function parameter
    Added a missing reference to the 'shapes' parameter in the Value function to avoid unnecessary copying and optimize performance. This ensures the function works more efficiently with the passed vector.

--- main.cpp
+++ main.cpp
@@ -525,7 +525,7 @@ public:
     //     return light.Value(point);
     // }
 
-    float Value(int x, int y, std::vector<Shape*> const shapes, Light light) {
+    float Value(int x, int y, std::vector<Shape*> const &shapes, Light light) {
         Vector3 startPosition = position;
         Vector3 endPosition = position + Vector3(-size / 2, -size * lines / columns / 2, fov) + Vector3(size * x / columns, size * y / lines, 0);
         // endPosition = (endPosition - position).Normalize() * maxDistance + startPosition;
